2a715699de4c7ef4010ac58907869e863a004ff0
Aleksandr Klimenkov <alx.devel@gmail.com>
2013-01-30 02:58:34 +0400

    fos user profile

--- src/Anyx/UserBundle/Controller/ProfileController.php
+++ src/Anyx/UserBundle/Controller/ProfileController.php
@@ -25,32 +25,6 @@ class ProfileController extends BaseController\ProfileController
     {
         return array();
     }
-
-    /**
-     * Edit the user
-     */
-    public function editAction(Request $request)
-    {
-        $user = $this->container->get('security.context')->getToken()->getUser();
-        if (!is_object($user) || !$user instanceof UserInterface) {
-            throw new AccessDeniedException('This user does not have access to this section.');
-        }
-
-        $form = $this->container->get('fos_user.profile.form');
-        $formHandler = $this->container->get('fos_user.profile.form.handler');
-
-        $process = $formHandler->process($user);
-        if ($process) {
-            $this->setFlash('fos_user_success', 'profile.flash.updated');
-
-            return new RedirectResponse($this->getRedirectionUrl($user));
-        }
-
-        return $this->container->get('templating')->renderResponse(
-                        'FOSUserBundle:Profile:edit.html.' . $this->container->getParameter('fos_user.template.engine'), array('form' => $form->createView())
-        );
-    }
-
     /**
      * @Route("/profile/created-crosswords/") 
      */
