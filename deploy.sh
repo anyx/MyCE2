576f85064169ea345543b041e1ef2f75cecf4d38
Aleksandr Klimenkov <alx.devel@gmail.com>
2013-02-10 00:49:36 +0400

    deply script

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+${1?"Укажите параметром окружение для которого производится развертывание. Например: ./deploy.sh prod"}
+composer install
+app/console doctrine:migrations:migrate --no-interaction --env=$1
+app/console assets:install --symlink --env=$1
+app/console assetic:dump --env=$1
+app/console cache:clear --env=$1
